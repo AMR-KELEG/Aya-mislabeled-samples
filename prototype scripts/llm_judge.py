a6938c6dd9ceeb73ee8ab8d6dc5c051e9c06dca9
hametar0u <57603310+hametar0u@users.noreply.github.com>
2024-08-10 17:53:49 -0700

    fix: convert notebook to script format

---
+++ prototype scripts/llm_judge.py
@@ -0,0 +1,56 @@
+# %%
+import cohere 
+import re
+import pandas as pd
+
+# %%
+co = cohere.Client(
+  api_key="", # This is your API key
+) 
+
+# Define rate limit: 10 calls per 60 seconds
+# CALLS = 5
+# PERIOD = 60
+
+# @sleep_and_retry
+# @limits(calls=CALLS, period=PERIOD)
+def eval(row):
+  inputprompt = row["inputs"]
+  answer = row["targets"]
+
+  print(inputprompt, answer)
+
+  response = co.chat( 
+    model='c4ai-aya-23-8b',
+    message=f'''[System]
+Please act as an impartial judge and evaluate the quality of the response provided by an
+AI assistant to the user question displayed below. Your evaluation should consider factors
+such as the helpfulness, relevance, accuracy, depth, creativity, and level of detail of
+the response. Begin your evaluation by providing a short explanation. Be as objective as
+possible. After providing your explanation, please rate the response on a scale of 1 to 10
+by strictly following this format: "[[rating]]", for example: "Rating: [[5]]".
+  [Input Prompt]
+  {inputprompt}
+  [The Start of Assistant’s Answer]
+  {answer}
+  [The End of Assistant’s Answer]''',
+  ) 
+  return response.text
+
+def extract_rating(s):
+    match = re.search(r'\[(\d+(\.\d+)?)\]', s)
+    if match:
+        return float(match.group(1))
+    return None
+
+# %%
+df = pd.read_csv("hf://datasets/mislabel-indentification-aya/aya_dataset_mislabeled/mislabeled_samples_CohereForAI_aya_dataset_train.csv")
+
+# %%
+df["llm_judge"] = df.apply(eval, axis=1)
+
+# %%
+df["rating"] = df["llm_judge"].apply(extract_rating)
+
+# %%
+df.to_csv("aya_mislabelled_with_evaluation.csv")
