8a9c58030bf1bbbd4876d61635cd59d088714553
hametar0u <57603310+hametar0u@users.noreply.github.com>
2024-08-18 07:50:57 -0700

    refactor: make preamble and model configurable parameters

--- prototype scripts/llm_judge.py
+++ prototype scripts/llm_judge.py
@@ -6,26 +6,39 @@ co = cohere.Client(
   api_key="", # This is your API key
 ) 
 
-def eval(row):
+def eval(row, model='command-r-plus', use_preamble=False):
   inputprompt = row["inputs"]
   answer = row["targets"]
 
-  print(inputprompt, answer)
-
-  response = co.chat( 
-    model='c4ai-aya-23-8b',
-    message=f'''[System]
+  instructions = '''[System]
 Please act as an impartial judge and evaluate the quality of the response provided by an
-AI assistant to the user question displayed below. Your evaluation should consider factors
+AI assistant in response to the user question displayed below. Your evaluation should consider factors
 such as the helpfulness, relevance, accuracy, depth, creativity, and level of detail of
 the response. Begin your evaluation by providing a short explanation. Be as objective as
 possible. Direct translations of the input prompt should NOT be rated highly. After providing your explanation, please rate the response on a scale of 1 to 10
-by strictly following this format: "[[rating]]", for example: "Rating: [[5]]".
-  [Input Prompt]
-  {inputprompt}
-  [The Start of Assistant’s Answer]
-  {answer}
-  [The End of Assistant’s Answer]''',
+by strictly following this format: "[[rating]]", for example: "Rating: [[5]]".'''
+
+  if use_preamble:
+    preamble = instructions
+    message = f'''
+    [Input Prompt]
+    {inputprompt}
+    [The Start of Assistant’s Answer]
+    {answer}
+    [The End of Assistant’s Answer]'''
+  else:
+    preamble = None
+    message = instructions + f'''
+    [Input Prompt]
+    {inputprompt}
+    [The Start of Assistant’s Answer]
+    {answer}
+    [The End of Assistant’s Answer]'''
+
+  response = co.chat( 
+    model=model,
+    message=message,
+    preamble=preamble,
   ) 
   return response.text
 
